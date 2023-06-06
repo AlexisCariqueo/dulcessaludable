de891b42e9961fc83f0299bbc3c1f9e014df85dd
acariqueo <acariqueo@vtsystems.cl>
2023-06-06 15:05:29 -0400

    cambios cart_items

--- database/migrations/2023_05_11_063232_create_cart_items_table.php
+++ database/migrations/2023_05_11_063232_create_cart_items_table.php
@@ -4,31 +4,34 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
+class CreateCartItemsTable extends Migration
 {
     /**
      * Run the migrations.
+     *
+     * @return void
      */
     public function up()
     {
         Schema::create('cart_items', function (Blueprint $table) {
-            $table->unsignedBigInteger('user_id')->after('id');
+            $table->id();
+            $table->unsignedBigInteger('user_id');
+            $table->unsignedBigInteger('productos_id');
+            $table->unsignedBigInteger('quantity');
+            $table->timestamps();
+
             $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
-            $table->unsignedBigInteger('product_id')->after('user_id');
-            $table->foreign('product_id')->references('id')->on('productos')->onDelete('cascade');
-            $table->dropForeign(['cart_id']);
-            $table->dropColumn('cart_id');
+            $table->foreign('productos_id')->references('id')->on('productos')->onDelete('cascade');
         });
     }
-    
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
     public function down()
     {
-        Schema::table('cart_items', function (Blueprint $table) {
-            $table->unsignedBigInteger('cart_id')->after('id');
-            $table->foreign('cart_id')->references('id')->on('carts')->onDelete('cascade');
-            $table->dropForeign(['user_id', 'product_id']);
-            $table->dropColumn('user_id', 'product_id');
-        });
+        Schema::dropIfExists('cart_items');
     }
-    
-};
+}
